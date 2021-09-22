7a10e564119bbae8cecb639e4f62861e43b8095e
KINGMJ <328047478@qq.com>
2021-09-22 17:19:45 +0800

    api-client新增post和upload方法

--- src/api/api-client.js
+++ src/api/api-client.js
@@ -3,13 +3,8 @@ import Constant from '../constant'
 
 axios.defaults.baseURL = Constant.API_URL
 axios.defaults.withCredentials = true
-/**
- *
- * @param {*} url
- * @param {*} params
- * @returns
- */
-export function get(url, params = {}) {
+
+export function get(url, params) {
   return new Promise((resolve, reject) => {
     axios
       .get(url, { params: params })
@@ -20,4 +15,40 @@ export function get(url, params = {}) {
         reject(error)
       })
   })
+}
+
+export function post(url, params) {
+  const { payload, isUpload } = params
+
+  console.log(isUpload)
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: {},
+      transformRequest: [
+        data => {
+          const formData = new FormData()
+          for (const item in data) {
+            formData.append(item, data[item])
+          }
+          return formData
+        },
+      ],
+    }
+    if (isUpload) {
+      options.headers = { 'Content-Type': 'multipart/form-data' }
+    }
+
+    axios
+      .post(url, payload, options)
+      .then(response => {
+        resolve(response.data)
+      })
+      .catch(error => {
+        reject(error)
+      })
+  })
+}
+
+export function upload(url, params) {
+  return post(url, { ...params, isUpload: true })
 }
