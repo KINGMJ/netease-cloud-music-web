e1d56d90648396a0da836f9571d472db13dbf17e
KINGMJ <328047478@qq.com>
2021-09-23 15:12:10 +0800

    删除不需要的文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
   {
     path: '/playlists/:playlistId',
     name: 'Playlists',
@@ -14,27 +8,9 @@ const routes = [
     component: () => import('../views/Playlist.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/upload',
-    name: 'FileUpload',
-    component: () => import('../views/FileUpload.vue'),
-  },
-  {
     path: '/cloud',
     name: 'Cloud',
     component: () => import('../views/Cloud.vue'),
-  },
-  {
-    path: '/home1',
-    name: 'Home1',
-    component: () => import('../views/Home1.vue'),
   },
 ]
 
