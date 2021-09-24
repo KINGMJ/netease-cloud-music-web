520e91f03a249cce8c18c3a214cfc0fb9180dfe5
KINGMJ <328047478@qq.com>
2021-09-24 11:33:33 +0800

    云盘过滤代码优化

--- src/views/Cloud.vue
+++ src/views/Cloud.vue
@@ -8,7 +8,12 @@
         <button class="btn">
           <CogIcon class="w-4 h-4" />
         </button>
-        <button class="btn" title="过滤出不在云盘中的歌曲" @click="$emit('handle-click-filter')">
+        <button
+          class="btn"
+          :class="filterOn ? 'filter-on' : ''"
+          title="过滤出不在歌单中的歌曲"
+          @click="filterOn = !filterOn"
+        >
           <FilterIcon class="w-4 h-4" />
         </button>
       </div>
@@ -26,33 +31,19 @@
           </thead>
           <tbody>
             <tr
-              v-for="(item, index) in cloudSongs"
+              v-for="(item, index) in filteredSongs"
               :key="item.songId"
               class="tr"
               :class="index % 2 === 0 ? 'bg-white' : 'bg-gray-50'"
-              @click="getCloudSongDetail(item.songId)"
             >
-              <td
-                class="px-6 py-4 whitespace-nowrap text-left font-medium text-gray-900 max-w-xs truncate"
-                :title="item.songName"
-              >
-                {{ item.songName }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-left font-medium text-gray-900 max-w-xs truncate">
-                {{ item.artist }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-left">
-                {{ (item.fileSize / 1000 / 1000).toFixed(1) + 'M' }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-left">
-                {{ item.fileName.split('.').pop().toLowerCase() }}
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-left">
+              <td class="td group-hover:text-green-500" :title="item.songName">{{ item.songName }}</td>
+              <td class="td group-hover:text-green-500">{{ item.artist }}</td>
+              <td class="td group-hover:text-green-500">{{ fileSizeFormat(item.fileSize) }}</td>
+              <td class="td group-hover:text-green-500">{{ fileTypeFormat(item.fileName) }}</td>
+              <td class="td group-hover:text-green-500">
                 <p v-for="playlist in item.playlists" :key="playlist.id">{{ playlist.name }}</p>
               </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-left">
-                {{ parseAddTime(item.addTime) }}
-              </td>
+              <td class="td group-hover:text-green-500">{{ timeFormat(item.addTime) }}</td>
             </tr>
           </tbody>
         </table>
@@ -62,18 +53,18 @@
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import { PencilAltIcon, CogIcon, FilterIcon, SortAscendingIcon } from '@heroicons/vue/outline'
+import useGetCloudSongs from '../composables/useGetCloudSongs'
+import useUtils from '../composables/useUtils'
 import Api from '../api'
 import _ from 'lodash'
-import dayjs from 'dayjs'
 
 export default defineComponent({
   components: {
     PencilAltIcon,
     CogIcon,
     FilterIcon,
-    // SortAscendingIcon,
   },
   setup() {
     const tableCol = [
@@ -84,63 +75,57 @@ export default defineComponent({
       { name: '所在歌单' },
       { name: '上传时间' },
     ]
-
-    // 获取云盘歌曲
-    const cloudSongs = ref([])
-    // 获取所有我创建的歌单的歌曲
+    // 云盘歌曲
+    const { cloudSongs } = useGetCloudSongs()
+    const { timeFormat, fileTypeFormat, fileSizeFormat } = useUtils()
+    // 所有我创建的歌单的歌曲
     const allPlayListSongs = ref([])
-    // 获取不在歌单中的歌曲
+    // 是否开启过滤
+    const filterOn = ref(false)
+    // 云盘歌曲，包含歌单信息
+    const cloudSongsWithPlaylist = ref([])
+    // 不在歌单中的云盘歌曲
     const notInPlaylistSongs = ref([])
-
     const uid = 372063478
 
     // 获取云盘的音乐
     const getCloudData = () => {
-      cloudSongs.value = []
-      Api.Cloud.getData({
-        limit: 1000,
-      }).then(res => {
-        cloudSongs.value = res.data.map(item => {
-          const index = _.findIndex(allPlayListSongs.value, o => {
-            return o.id == item.songId
-          })
-          if (~index) {
-            return {
-              songId: item.songId,
-              songName: item.songName,
-              artist: item.artist,
-              fileSize: item.fileSize,
-              fileName: item.fileName,
-              addTime: item.addTime,
-              inPlaylist: true,
-              playlists: allPlayListSongs.value[index].playlist,
-            }
-          } else {
-            const song = {
-              songId: item.songId,
-              songName: item.songName,
-              artist: item.artist,
-              fileSize: item.fileSize,
-              fileName: item.fileName,
-              addTime: item.addTime,
-              inPlaylist: false,
-              playlists: [],
-            }
-            notInPlaylistSongs.value.push(song)
-            return song
-          }
+      cloudSongsWithPlaylist.value = cloudSongs.value.map(item => {
+        const index = _.findIndex(allPlayListSongs.value, o => {
+          return o.id == item.songId
         })
-        console.log(cloudSongs.value)
+        if (~index) {
+          return {
+            songId: item.songId,
+            songName: item.songName,
+            artist: item.artist,
+            fileSize: item.fileSize,
+            fileName: item.fileName,
+            addTime: item.addTime,
+            inPlaylist: true,
+            playlists: allPlayListSongs.value[index].playlist,
+          }
+        } else {
+          const song = {
+            songId: item.songId,
+            songName: item.songName,
+            artist: item.artist,
+            fileSize: item.fileSize,
+            fileName: item.fileName,
+            addTime: item.addTime,
+            inPlaylist: false,
+            playlists: [],
+          }
+          notInPlaylistSongs.value.push(song)
+          return song
+        }
       })
     }
 
-    const getCloudSongDetail = songId => {
-      Api.Cloud.getSongDetail({
-        song_id: songId,
-      }).then(res => {
-        console.log(res)
-      })
-    }
+    // 云盘歌曲过滤，过滤出不在歌单中的歌曲
+    const filteredSongs = computed(() => {
+      return filterOn.value ? notInPlaylistSongs.value : cloudSongsWithPlaylist.value
+    })
 
     const sortSongsBySize = () => {
       cloudSongs.value.sort((a, b) => {
@@ -163,72 +148,54 @@ export default defineComponent({
     }
 
     // 获取我创建的歌单里的所有歌曲
-    const getAllPlayListSongs = () => {
-      allPlayListSongs.value = []
-      Api.User.getPlaylist({
-        uid: uid,
-      }).then(res => {
-        // 获取我创建的歌单
-        const playlistIds = res.playlist.reduce((prev, current) => {
-          if (current.creator.userId == uid) {
-            prev.push(current.id)
-          }
-          return prev
-        }, [])
-
-        for (let id of playlistIds) {
-          Api.PlayList.getSongs({
-            playlist_id: id,
-          }).then(res => {
-            console.log(res)
-            const playlist = res.playlist
-            const trackIds = playlist.trackIds
-
-            for (let item of trackIds) {
-              const index = _.findIndex(allPlayListSongs.value, o => {
-                return o.id == item.id
-              })
-              if (~index) {
-                allPlayListSongs.value[index].playlist.push({ id: playlist.id, name: playlist.name })
-              } else {
-                allPlayListSongs.value.push({ id: item.id, playlist: [{ id: playlist.id, name: playlist.name }] })
-              }
-            }
-            console.log(allPlayListSongs.value)
-          })
+    const getAllPlayListSongs = async () => {
+      const res = await Api.User.getPlaylist({ uid })
+      // 获取我创建的歌单
+      const playlistIds = res.playlist.reduce((prev, current) => {
+        if (current.creator.userId == uid) {
+          prev.push(current.id)
         }
-      })
+        return prev
+      }, [])
+
+      for (let id of playlistIds) {
+        const res = await Api.PlayList.getSongs({ playlist_id: id })
+        const playlist = res.playlist
+        const trackIds = playlist.trackIds
+
+        for (let item of trackIds) {
+          const index = _.findIndex(allPlayListSongs.value, o => {
+            return o.id == item.id
+          })
+          if (~index) {
+            allPlayListSongs.value[index].playlist.push({ id: playlist.id, name: playlist.name })
+          } else {
+            allPlayListSongs.value.push({ id: item.id, playlist: [{ id: playlist.id, name: playlist.name }] })
+          }
+        }
+      }
     }
 
-    // 过滤出不在歌单中的歌曲
-    const filterNotInPlaylistSongs = () => {
-      cloudSongs.value = notInPlaylistSongs.value
+    const bootstrap = () => {
+      getAllPlayListSongs()
+      setTimeout(() => {
+        getCloudData()
+      }, 2000)
     }
 
-    const parseAddTime = time => {
-      console.log(time)
-
-      console.log(dayjs(new Date(time)).format('YYYY-MM-ddTHH:mm:ss'))
-      return dayjs(new Date(time)).format('YYYY-MM-DD HH:mm')
-    }
-
-    getAllPlayListSongs()
-
-    setTimeout(() => {
-      getCloudData()
-    }, 2000)
+    bootstrap()
 
     return {
-      cloudSongs,
-      allPlayListSongs,
+      tableCol,
       getCloudData,
-      getCloudSongDetail,
       sortSongsBySize,
       sortSongsByType,
       getAllPlayListSongs,
-      filterNotInPlaylistSongs,
-      parseAddTime,
-      tableCol,
+      filteredSongs,
+      timeFormat,
+      fileTypeFormat,
+      fileSizeFormat,
+      filterOn,
     }
   },
 })
@@ -243,11 +210,15 @@ export default defineComponent({
   @apply focus:outline-none h-6 text-sm leading-none text-gray-800  border-b border-t border-gray-100 group;
 }
 
-.badge {
-  @apply px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 text-left group-hover:text-green-500;
-}
-
 .btn {
   @apply mr-2 p-2 text-gray-500 border-transparent border bg-gray-100 hover:bg-gray-200 rounded focus:outline-none;
+}
+
+.btn.filter-on {
+  @apply bg-green-400 hover:bg-green-500 text-white;
+}
+
+.td {
+  @apply px-6 py-4 whitespace-nowrap text-left font-medium text-gray-900 max-w-xs truncate;
 }
 </style>
