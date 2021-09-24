5241c84df5326adb6012974a650f7cdab94a03ba
KINGMJ <328047478@qq.com>
2021-09-24 11:44:10 +0800

    歌单列表文字颜色调整

--- src/components/PlaylistSongs.vue
+++ src/components/PlaylistSongs.vue
@@ -18,30 +18,30 @@
                 class="tr"
                 :class="index % 2 === 0 ? 'bg-white' : 'bg-gray-50'"
               >
-                <td class="px-6 py-4 whitespace-nowrap text-left">
+                <td class="td">
                   <div class="flex items-center">
-                    <div class="text-sm font-medium text-gray-900 group-hover:text-green-500 flex items-center">
+                    <div class="font-medium text-gray-900 group-hover:text-green-500 flex items-center">
                       <img class="h-9 w-9 rounded-lg mr-3" :src="item.al.picUrl" alt="" />
                       {{ index + 1 + '. ' + item.name }}
                       <!-- ({{ item.id }}) -->
                     </div>
                   </div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-left">
-                  <div class="text-sm text-gray-900 group-hover:text-green-500">{{ item.ar[0].name }}</div>
+                <td class="td">
+                  <div class="text-gray-900 group-hover:text-green-500">{{ item.ar[0].name }}</div>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
+                <td class="td text-gray-500">
                   <span class="badge">
                     {{ item.inCloud ? '是' : '' }}
                   </span>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-left group-hover:text-green-500">
+                <td class="td text-gray-500 group-hover:text-green-500">
                   {{ fileSizeFormat(item.fileSize) }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-left group-hover:text-green-500">
+                <td class="td text-gray-500 group-hover:text-green-500">
                   {{ fileTypeFormat(item.fileName) }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-xs text-left cursor-pointer group-hover:text-green-500">
+                <td class="td cursor-pointer group-hover:text-green-500">
                   <CloudUploadIcon
                     v-if="!item.inCloud"
                     class="w-5 h-5 opacity-0 group-hover:opacity-100"
@@ -90,10 +90,14 @@ export default defineComponent({
 }
 
 .tr {
-  @apply focus:outline-none h-6 text-sm leading-none text-gray-800  border-b border-t border-gray-100 group;
+  @apply focus:outline-none h-6 text-sm leading-none border-b border-t border-gray-100 group;
 }
 
 .badge {
   @apply px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 text-left group-hover:text-green-500;
+}
+
+.td {
+  @apply px-6 py-4 whitespace-nowrap text-left text-sm;
 }
 </style>
