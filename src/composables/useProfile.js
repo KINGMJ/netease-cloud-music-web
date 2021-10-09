c627f8a887c7f52bb78bca545283c4c73d004a5e
KINGMJ <328047478@qq.com>
2021-10-09 15:34:29 +0800

    用户信息保存在cookie中

--- src/composables/useProfile.js
+++ src/composables/useProfile.js
@@ -1,11 +1,20 @@
 import { reactive, readonly, toRefs } from 'vue'
+import Cookies from 'js-cookie'
+import Constant from '../constant'
+
+// 从cookie中取出用户profile
+let profileFromCookie = Cookies.get(Constant.COOKIE_PROFILE_KEY)
+// eslint-disable-next-line no-extra-boolean-cast
+profileFromCookie = !!profileFromCookie
+  ? JSON.parse(profileFromCookie)
+  : {
+      uid: null,
+      nickname: '',
+      avatarUrl: '',
+    }
 
 //用户属性
-const initProfile = reactive({
-  uid: null,
-  nickname: '',
-  avatarUrl: '',
-})
+const initProfile = reactive(profileFromCookie)
 
 const profile = readonly(initProfile)
 
@@ -14,6 +23,14 @@ export default function useProfile() {
     initProfile.uid = userId
     initProfile.nickname = nickname
     initProfile.avatarUrl = avatarUrl
+    Cookies.set(
+      Constant.COOKIE_PROFILE_KEY,
+      JSON.stringify({
+        uid: userId,
+        nickname,
+        avatarUrl,
+      })
+    )
   }
 
   return {
